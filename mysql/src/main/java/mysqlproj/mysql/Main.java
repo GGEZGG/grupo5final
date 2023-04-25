5302dd3e34bd17eef752625385cf15f1d1ffde43
GGEZGG <gaston.ez.g@gmail.com>
2023-04-24 21:49:13 -0300

    limpieza

--- mysql/src/main/java/mysqlproj/mysql/Main.java
+++ mysql/src/main/java/mysqlproj/mysql/Main.java
@@ -25,11 +25,10 @@ public class Main {
     	String sURL = "jdbc:mysql://localhost:3306/trabajo_integrador"; 
     	Class.forName("com.mysql.cj.jdbc.Driver"); 
     	try { 
-    		conectar = DriverManager.getConnection(sURL,"root","caca442023"); 
+    		conectar = DriverManager.getConnection(sURL,"root","123456"); 
     	JOptionPane.showMessageDialog(null,"Conexion Exitosa"); 
     	} catch (SQLException ex) { 
     	JOptionPane.showMessageDialog(null, "conexion fallida");//
-    	//Logger.getLogger(PruebaConexion.class.getName()).log(Level.SEVERE, null, ex); 
     	}
 
         Statement s=null;
@@ -37,7 +36,6 @@ public class Main {
            s = conectar.createStatement();
        } 
    catch (SQLException ex) {
-         //  Logger.getLogger(PruebaConexion.class.getName()).log(Level.SEVERE, null, ex);
       
        }
       
@@ -47,37 +45,21 @@ public class Main {
            ResultSet rpron=null;
            ResultSet requip=null;
 
+   		//====EQUIPO====
+
    try {
-          // rs = s.executeQuery("select * from facturas where cod_factura='"+val+"'");//equipo");
-       	//  rs = s.executeQuery("SELECT * FROM equipo"); 
 	   rs = s.executeQuery("(SELECT * FROM trabajo_integrador.resultado as resultados inner join  trabajo_integrador.equipo as equipo2 on resultados.cod_equipo1=equipo2.cod_equipo inner join trabajo_integrador.equipo as equipo3   \r\n"
 	   		+ "\r\n"
 	   		+ " on (  resultados.cod_equipo2=equipo3.cod_equipo) )   ;"); 
        
-        JOptionPane.showMessageDialog(null,"Consulta realizada");
+        JOptionPane.showMessageDialog(null,"Consulta realizada-EQUIPO");
        } 
    catch (SQLException ex) {
-          // Logger.getLogger(PruebaConexion.class.getName()).log(Level.SEVERE, null, ex);
        
-   	 JOptionPane.showMessageDialog(null,"Consulta fallida");
+   	 JOptionPane.showMessageDialog(null,"Consulta fallida-EQUIPO");
        }
        
-      
-  
-    /*   
-   	try {
-			while(rs.next())
-			    
-			{
-	System.out.println(    	rs.getString(7));
 
-			
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-    	
    Partido rr=null;
    Pronostico rp=null;
 
@@ -87,54 +69,43 @@ public class Main {
    Equipo equipo1;
    Equipo equipo2;
   
-try {
-   while(rs.next())
+   try {
+	   while(rs.next())
        
-   {
-  rr=new Partido();
-   	equipo1=new Equipo();
+	   {
+		   rr=new Partido();
+		   equipo1=new Equipo();
    	
-  equipo2=new Equipo();     
+		   equipo2=new Equipo();     
   
-  equipo1.setNombre(rs.getString(7));
-  equipo2.setNombre(rs.getString(10));
-//JOptionPane.showMessageDialog(null, rs.getString(10));
+		   equipo1.setNombre(rs.getString(7));
+		   equipo2.setNombre(rs.getString(10));
 
   
-  rr.setEquipo1(equipo1);
-  rr.setEquipo2(equipo2);
-  rr.setGolesEquipo1(rs.getInt(3));
-  rr.setGolesEquipo2(rs.getInt(4));
+		   rr.setEquipo1(equipo1);
+		   rr.setEquipo2(equipo2);
+		   rr.setGolesEquipo1(rs.getInt(3));
+		   rr.setGolesEquipo2(rs.getInt(4));
   
-  arrayPartidos.add(rr);
-  //System.out.println(rs.getString(7)+"  "+rs.getInt(3)+"  "+rs.getInt(4)+" "+rs.getString(10));
+		   arrayPartidos.add(rr);
      
-   }
-} catch (SQLException ex) {
-//   Logger.getLogger(PruebaConexion.class.getName()).log(Level.SEVERE, null, ex);
+	   }
+   } catch (SQLException ex) {
+	   //   Logger.getLogger(PruebaConexion.class.getName()).log(Level.SEVERE, null, ex);
+	   JOptionPane.showMessageDialog(null,"ERROR" );
 
-}
+   		}
 
-	//TEST===
-
-	for(i=0;i<arrayPartidos.size();i++) {
-		JOptionPane.showMessageDialog(null, arrayPartidos.get(i).getEquipo1().getNombre());
-	}
-	
-	
-	JOptionPane.showMessageDialog(null,"aaa" );
-
-	
-	//PRONOSTICO===========================
+//====PRONOSTICO====
 	
 	
 	   try {
 		   rpron = s.executeQuery("SELECT * FROM pronostico");
 
-	        JOptionPane.showMessageDialog(null,"Consulta realizada");
+	        JOptionPane.showMessageDialog(null,"Consulta realizada-PRONOSTICO");
 	       } 
 	   catch (SQLException ex) {
-	   	 JOptionPane.showMessageDialog(null,"Consulta fallida");
+	   	 JOptionPane.showMessageDialog(null,"Consulta fallida-PRONOSTICO");
 	       }
 
 	   
@@ -148,20 +119,9 @@ try {
 		   	
 		  equipo2=new Equipo();
 
-		  //requip.absolute(Integer.parseInt(rpron.getString(2)));
-		  //equipo1.setNombre(rpron.getString(2));
 		  equipo1.setNombre(arrayPartidos.get(rpron.getRow()-1).getEquipo1().getNombre());
 		  equipo2.setNombre(arrayPartidos.get(rpron.getRow()-1).getEquipo2().getNombre());
 
-		  //arrayPartidos.get(Integer.parseInt(rpron.getString(2))).getEquipo1().getNombre();
-				  //Integer.parseInt((rpron.getString(2))).getEquipo1().getNombre()
-		  //requip.absolute(Integer.parseInt(rpron.getString(6)));
-		  //equipo2.setNombre(rpron.getString(6));
-
-		  //equipo1.setNombre(rs.getString(7));
-		  //equipo2.setNombre(rs.getString(10));
-
-		  
 		  rp.setEquipo1(equipo1);
 		  rp.setEquipo2(equipo2);
 		  rp.setGanaEquipo1(rpron.getInt(3));
@@ -172,16 +132,12 @@ try {
 
 		   }
 		} catch (SQLException ex) {
-		//   Logger.getLogger(PruebaConexion.class.getName()).log(Level.SEVERE, null, ex);
-			JOptionPane.showMessageDialog(null,"error" );
+			JOptionPane.showMessageDialog(null,"ERROR" );
 			ex.printStackTrace();
 
 		}
-    
-	   
-		for(i=0;i<arrayPronosticos.size();i++) {
-			JOptionPane.showMessageDialog(null, arrayPronosticos.get(i).getEquipo1().getNombre());
-		}
+		//====CALCULO DE PUNTAJE====
+
 		int puntaje=0;
 		
 		for( i = 0; i < arrayPronosticos.size(); i++) {
@@ -203,6 +159,5 @@ try {
 		
 		JOptionPane.showMessageDialog(null, "El puntaje es " + puntaje);
 		
-    	
     }
 }
